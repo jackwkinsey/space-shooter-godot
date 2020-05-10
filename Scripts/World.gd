40b3e5c39c48c1047148dafb9fdb43b82e110fb4
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-10 17:45:54 -0400

    feat: start star particles on ready

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,6 +1,7 @@
 extends Node
 
 onready var score_label = $ScoreLabel
+onready var star_particles = $StarParticles
 
 var score = 0 setget set_score
 
@@ -8,5 +9,9 @@ func set_score(value):
 	score = value
 	score_label.text = "Score: " + str(score)
 
+func _ready():
+	star_particles.emitting = true
+
+# Signals
 func _on_Enemy_enemy_killed(points):
 	self.score += points
