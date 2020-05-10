dd9f77874c4e9ae0727c2af440ceec0d5c0316e8
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-10 17:45:28 -0400

    refactor: only play explosion on death
    - use area_entered signal instead of body

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -17,21 +17,22 @@ func _process(delta):
 	position.x -= SPEED * delta;
 
 func _exit_tree():
-	var explosion_effect = ExplosionEffect.instance()
-	var main = get_tree().current_scene
-	var field = main.get_node("Field")
-	
-	field.call_deferred("add_child", explosion_effect)
-	explosion_effect.global_position = global_position
+	if HEALTH <= 0:
+		var explosion_effect = ExplosionEffect.instance()
+		var main = get_tree().current_scene
+		var field = main.get_node("Field")
+		
+		field.call_deferred("add_child", explosion_effect)
+		explosion_effect.global_position = global_position
 
 # Signals
-func _on_Enemy_body_entered(body):
-	body.queue_free()
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_Enemy_area_entered(area):
+	area.queue_free()
 	
 	HEALTH -= 1
 	if HEALTH <= 0:
 		emit_signal("enemy_killed", POINTS)
 		queue_free()
-
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
