2f55c6b7048679f0db157513a0c622662cc50191
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-10 23:05:35 -0400

    feat: add logic to start or quit game on key press

--- Scripts/StartMenu.gd
+++ Scripts/StartMenu.gd
@@ -1,7 +1,16 @@
 extends Node
 
-func _on_StartButton_pressed():
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		start_game()
+	if Input.is_action_just_pressed("ui_cancel"):
+		quit_game()
+
+func start_game():
 	var error_code = get_tree().change_scene("res://Scenes/World.tscn")
 	
 	if error_code != 0:
 		print("Error while loading scene: " + str(error_code))
+
+func quit_game():
+	get_tree().quit();
