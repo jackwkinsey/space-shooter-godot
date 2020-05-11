f68d37f1eb7c3ac44d69c2eb0d7f37c280656517
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-11 00:12:13 -0400

    feat: load highscore on game over
    Update highscore label with loaded score

--- Scripts/GameOverScreen.gd
+++ Scripts/GameOverScreen.gd
@@ -1,5 +1,10 @@
 extends Node
 
+onready var highscore_label = $HighscoreLabel
+
+func _ready():
+	set_highscore_label()
+
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		start_game()
@@ -17,3 +22,7 @@ func go_to_main_menu():
 	
 	if error_code != 0:
 		print("Error while loading scene: " + str(error_code))
+
+func set_highscore_label():
+	var save_data = SaveAndLoad.load_data_from_file()
+	highscore_label.text = "Highscore: " + str(save_data.highscore)
