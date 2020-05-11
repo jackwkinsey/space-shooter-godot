760cc8c0cd59e212118f4d74b716fb7091977c47
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-05-10 23:33:11 -0400

    feat: listen for ship destroyed signal
    When the ship is destroyed:
    - wait for 1 second so the explosion effect plays
    - change to Game Over screen

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -15,3 +15,9 @@ func _ready():
 # Signals
 func _on_Enemy_enemy_killed(points):
 	self.score += points
+
+func _on_Ship_ship_destroyed():
+	yield(get_tree().create_timer(1), "timeout")
+	var error_code = get_tree().change_scene("res://Scenes/GameOverScreen.tscn")
+	if error_code != 0:
+		print("Error while loading scene: " + str(error_code))
